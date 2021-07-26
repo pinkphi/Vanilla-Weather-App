e95eaf633fae04f5e1238a5110b6954c0803ce70
PamelaUribe <pamela.u.a@gmail.com>
2021-07-26 00:12:24 -0500

    Added search engine

--- src/app.js
+++ src/app.js
@@ -45,9 +45,19 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
+function search(city) {
+  let apiKey = "9450c4e093311119f00946463ff53dcd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "9450c4e093311119f00946463ff53dcd";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+search("New York");
+
+let form = document.querySelector(".search-form");
+form.addEventListener("submit", handleSubmit);
